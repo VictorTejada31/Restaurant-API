8907ce443328aa861e1ebb670f5981450410a7be
juanetes02 <131911600+juanetes02@users.noreply.github.com>
2023-05-15 22:34:43 -0400

    Application Context seed added

---
+++ Restaurant.Infrastructure.Persistence/Seeds/DefaultDishCategories.cs
@@ -0,0 +1,28 @@
+﻿using Restaurant.Core.Domain.Entities;
+using Restaurant.Infrastructure.Persistence.Repositories;
+
+namespace Restaurant.Infrastructure.Persistence.Seeds
+{
+    public static class DefaultDishCategories
+    {
+        public static async void AddAsync(DishCategoryRepository repository)
+        {
+            DishCategory drink = new() { Name = "Drink"};
+            DishCategory main = new() { Name = "Main Dish" };
+            DishCategory entree = new() { Name = "Entree" };
+
+            var dishes = await repository.GetAllAsync(); 
+
+            if(dishes.All(dish => dish.Id != drink.Id))
+            {
+                await repository.AddAsync(drink);
+                await repository.AddAsync(main);
+                await repository.AddAsync(entree);
+
+            }
+
+
+
+        }
+    }
+}
